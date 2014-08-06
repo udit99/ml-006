cae013c8cf4f83e209478a523b9240129e4756f8
Udit Manektala <udit.manektala@getbraintree.com>
2014-08-05 19:19:42 -0700

    making the gradient work by copying earlier homework...not sure I totally understand/remember what I did there

--- mlclass-ex5-006/mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5-006/mlclass-ex5/linearRegCostFunction.m
@@ -15,6 +15,31 @@ grad = zeros(size(theta));
 hx = X * theta;
 J = (sum((hx - y).^2))/(2 * m) + sum((theta(2:end,:)).^2) * lambda/(2*m);
 
+
+ %j= 0
+
+%X0 = X(:,1);
+%theta0 = theta(1,:);
+%hx0 = X0 * theta0;
+%cost0 = (hx0 - y);
+%grad0 = (cost0' * X0)/m
+%grad0 = -10.218
+
+%j =1
+
+%Xrest = X(:, 2:end);
+%thetarest = theta(2:end,:);
+%hxrest = Xrest * thetarest;
+%costrest = hxrest - y;
+%gradrest = sum(costrest' * Xrest)/m + (theta * lambda )/m
+
+hx = (X * theta );
+hxone = (X * theta );
+grad = (X' * (hx - y))./m + (lambda/m * theta);
+grad_with_one = (X' * (hxone - y))./m;
+grad(1) = grad_with_one(1);
+
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
 %               regression for a particular choice of theta.
